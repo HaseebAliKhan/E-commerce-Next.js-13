c3289f18c54a9c7a58741a45af81913023e67504
HaseebAliKhan <57507467+HaseebAliKhan@users.noreply.github.com>
2023-07-15 22:26:41 +0500

    New start

---
+++ sanity/product.ts
@@ -0,0 +1,12 @@
+export const product = {
+    name:"product",
+    title:"Product",
+    type:"document",
+    fields:[
+        {
+            name:"title",
+            title:"Title",
+            type:"string"
+        }
+    ]
+}
